70e8fc9185460f93e308946c24162ec7ce8c4264
susum <susumeyyea@gmail.com>
2023-05-05 13:49:09 +0300

    eda jupyter notebook

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,3 +5,8 @@ Created on Thu May  4 11:17:03 2023
 @author: susum
 """
 
+import pandas as pd
+
+df= pd.read_csv("C:/Users/susum/Documents/ds_happiness_proj/world-happiness-report-2021.csv")
+df.rename(columns={'Country name': 'Country'}, inplace=True)
+df.rename(columns={'Regional indicator': 'Region'}, inplace=True)
